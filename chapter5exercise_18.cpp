1d8c04adbe35e979cacb9aede50359f7c15f8413
BLAKEJEWELL <bjewell1833@gmail.com>
2025-05-09 10:43:30 -0400

    Add project files.

---
+++ chapter5exercise_18.cpp
@@ -0,0 +1,92 @@
+/*
+File name: chapter5exercise18.cpp
+Programmer: Blake Jewell
+Date: 05/25
+Requirements: Write a program that produces a bar chart showing the population growth of Prairieville,
+a small town in the Midwest, at 20-year intervals during the past 100 years. The program should read in
+the population figures (rounded to the nearest 1,000 people) for 1900, 1920, 1940, 1960, 1980, and 2000
+from a file. For each year, it should display the date and a bar consisting of one asterisk for each 1,000 people.
+*/
+
+using namespace std;
+
+#include <iostream>
+#include <fstream>
+#include<string>
+
+void getnumbers(long int [],int, string);
+void displayresults(long int[], int);
+int choose();
+string getfilename();
+void getfilehandle(string);
+
+const string location = R"(C:\Users\Blake Jewell\Downloads\People.txt)";
+
+int main()
+{
+    int choice;
+    do {
+        const int peryear = 6;
+        long int mainarray[peryear];
+        string file = getfilename();
+        getfilehandle(file);
+        getnumbers(mainarray, peryear, file);
+        displayresults(mainarray, peryear);
+        choice = choose();
+    } while (choice == 1);
+}
+void getnumbers(long int array[], int num, string file)
+{
+    ifstream inputfile;
+    inputfile.open(file);
+    for (int count = 0; count < num;count++)
+    {
+        inputfile >> array[count];
+    }
+    inputfile.close();
+}
+void displayresults(long int array[], int year)
+{
+    cout << "PRAIRIEVILLE POPULATION GROWTH " << endl;
+    cout << "(each * represents 1,000 people)" << endl;
+    int thousands;
+    int time = 1900;
+    for (int count = 0; count < year;count++)
+    {
+        cout << time + (count * 20);
+        thousands = array[count] / 1000;
+        for (int index = 0;index < thousands;index++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
+int choose()
+{
+    int choice;
+    cout << "Would you like to run the program again? 1 for yes or 2 for no" << endl;
+    cin >> choice;
+    while (choice < 1 || choice >2)
+    {
+        cout << "Invalid selection. Please select either 1 or 2." << endl;
+        cin >> choice;
+    }
+    return choice;
+}
+string getfilename()
+{
+    string filename;
+    cout << "please enter the full name of the file" << endl;
+    cout<< R"(C:\Users\Blake Jewell\Downloads\People.txt)"<<endl;
+    getline(cin, filename);
+    return filename;
+}
+void getfilehandle(string name)
+{
+    ifstream handle;
+    handle.open(name);
+    if (!handle.is_open())
+        cout << "could not find the file" << endl;
+}
